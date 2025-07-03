ae8322d31259c67a59e217625f72643c51a825d5
riicardojs <ricardo.jeronymo@hotmail.com>
2025-07-03 01:27:29 -0300

    feat: integrate framer-motion for animations in upload widget components

--- src/components/upload-widget-upload-item.tsx
+++ src/components/upload-widget-upload-item.tsx
@@ -1,10 +1,16 @@
+import * as Progress from '@radix-ui/react-progress';
 import { Download, ImageUp, Link2, X } from 'lucide-react';
+import { motion } from 'motion/react';
 import { Button } from './ui/button';
-import * as Progress from '@radix-ui/react-progress'
 
 export function UploadWidgetUploadItem() {
   return (
-    <div className="p-3 rounded-lg flex flex-col gap-3 shadow-shape-content bg-white/3 relative overflow-hidden">
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.4 }}
+      className="p-3 rounded-lg flex flex-col gap-3 shadow-shape-content bg-white/3 relative overflow-hidden"
+    >
       <div className="flex flex-col gap-1">
         <span className="text-xs font-medium flex items-center gap-1">
           <ImageUp className="size-3 text-zinc-300" strokeWidth={1.5} />
@@ -23,8 +29,11 @@ export function UploadWidgetUploadItem() {
         </span>
       </div>
 
-      <Progress.Root className='bg-zinc-800 rounded-full h-1 overflow-hidden'>
-        <Progress.Indicator className='bg-indigo-500 h-1' style={{ width: '45%' }} />
+      <Progress.Root className="bg-zinc-800 rounded-full h-1 overflow-hidden">
+        <Progress.Indicator
+          className="bg-indigo-500 h-1"
+          style={{ width: '45%' }}
+        />
       </Progress.Root>
 
       <div className="absolute top-2.5 right-2.5 flex items-center gap-1">
@@ -41,6 +50,6 @@ export function UploadWidgetUploadItem() {
           <span className="sr-only">Cancel Upload</span>
         </Button>
       </div>
-    </div>
+    </motion.div>
   );
 }
--- src/components/upload-widget.tsx
+++ src/components/upload-widget.tsx
@@ -1,18 +1,28 @@
 import * as Collapsible from '@radix-ui/react-collapsible';
+import { motion, useCycle } from 'motion/react';
 import { UploadWidgetDropzone } from './upload-widget-dropzone';
 import { UploadWidgetHeader } from './upload-widget-header';
-import { UploadWidgetUploadList } from './upload-widget-upload-list';
 import { UploadWidgetMinimizedButton } from './upload-widget-minimized-button';
-import { useState } from 'react';
+import { UploadWidgetUploadList } from './upload-widget-upload-list';
 
 export function UploadWidget() {
-    const [ isWidgetOpen, setIsWidgetOpen ] = useState(false);
+  const [isWidgetOpen, toggleIsWidgetOpen] = useCycle(false, true);
+
   return (
-    <Collapsible.Root onOpenChange={setIsWidgetOpen}>
-      <div className="bg-zinc-900 overflow-hidden w-[360px] rounded-xl shadow-shape">
-        {!isWidgetOpen && (
-            <UploadWidgetMinimizedButton/>
-        )}
+    <Collapsible.Root onOpenChange={() => toggleIsWidgetOpen()}>
+      <motion.div
+        className="bg-zinc-900 overflow-hidden max-w-[360px] rounded-xl shadow-shape"
+        animate={isWidgetOpen ? 'open' : 'closed'}
+        variants={{
+          closed: {
+            width: 'max-content',
+            height: 44,
+            transition: { type: 'inertia' },
+          },
+          open: { width: 360, height: 'auto', transition: { duration: 0.1 } },
+        }}
+      >
+        {!isWidgetOpen && <UploadWidgetMinimizedButton />}
         <Collapsible.Content>
           <UploadWidgetHeader />
 
@@ -24,7 +34,7 @@ export function UploadWidget() {
             <UploadWidgetUploadList />
           </div>
         </Collapsible.Content>
-      </div>
+      </motion.div>
     </Collapsible.Root>
   );
 }
