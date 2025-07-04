57a1a5604c5956d774f4ca8b5aae76c71b182103
riicardojs <ricardo.jeronymo@hotmail.com>
2025-07-04 00:09:13 -0300

    feat: add uploadFileToStorage function and integrate it into upload management

--- src/store/uploads.ts
+++ src/store/uploads.ts
@@ -1,6 +1,7 @@
 import { enableMapSet } from 'immer';
 import { create } from 'zustand';
 import { immer } from 'zustand/middleware/immer';
+import { uploadFileToStorage } from '../http/upload-file-to-storage';
 
 export type Upload = {
   name: string;
@@ -16,6 +17,15 @@ enableMapSet();
 
 export const useUploads = create<UploadState, [['zustand/immer', never]]>(
   immer((set, get) => {
+    async function processUpload(uploadId: string) {
+      const upload = get().uploads.get(uploadId);
+      if (!upload) {
+        return;
+      }
+
+      await uploadFileToStorage({ file: upload.file });
+    }
+
     function addUpload(files: File[]) {
       for (const file of files) {
         const uploadId = crypto.randomUUID();
@@ -28,6 +38,8 @@ export const useUploads = create<UploadState, [['zustand/immer', never]]>(
         set((state) => {
           state.uploads.set(uploadId, upload);
         });
+
+        processUpload(uploadId);
       }
     }
     return {
